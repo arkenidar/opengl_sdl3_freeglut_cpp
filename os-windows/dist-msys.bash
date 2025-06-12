24a666d473b6ce56ff1269e31ca688cde0f2dd47
Dario Cangialosi <dario.cangialosi@gmail.com>
2025-06-12 23:43:19 +0200

    Add Windows distribution files including app.exe, SDL3.dll, libfreeglut.dll, and deployment script

--- os-windows/dist-msys.bash
+++ os-windows/dist-msys.bash
@@ -1,4 +1,6 @@
-cp ../../build/app.exe .
-deplister.exe app.exe
+#!/bin/bash
+
+cp ../build/app.exe .
+./deplister.exe app.exe
 cp /c/msys64/mingw64/bin/SDL3.dll .
 cp /c/msys64/mingw64/bin/libfreeglut.dll .
